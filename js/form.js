6518773d1acba42694efd03e0706b530c604c903
t0rn1k3 <tornikeburjanadze2@gmail.com>
2023-02-03 23:48:01 +0400

    add position and education parts of form and add styles on  them

---
+++ js/form.js
@@ -0,0 +1,9 @@
+const form = document.getElementById('form');
+const fName = document.getElementById('firstName');
+const lName = document.getElementById('lastName');
+
+
+
+// form validation
+
+
---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+const welcomePage = document.getElementById('welcomePage');
+const personalInfo = document.getElementById('personalInfo');
+const start = document.getElementById('buttonStart');
+
