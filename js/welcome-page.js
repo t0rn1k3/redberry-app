1c0f2e64083a080ac5eaaa5e927e32e53d198acb
t0rn1k3 <tornikeburjanadze2@gmail.com>
2023-02-03 18:38:21 +0400

    create first page inputs and add styles on them

--- js/welcome-page.js
+++ js/welcome-page.js
@@ -2,7 +2,7 @@ const welcomePage = document.getElementById('welcomePage');
 const start = document.getElementById('buttonStart');
 
 
-//activate add resume button on click
+//activate add-resume button on click
 
 start.addEventListener('click', () => {
     welcomePage.style.display = 'none';
