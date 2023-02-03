e7af44eea70cb3431e0e0624f8e4c19a8a1df469
t0rn1k3 <tornikeburjanadze2@gmail.com>
2023-02-03 16:15:32 +0400

    create welcome page with start button

---
+++ js/welcome-page.js
@@ -0,0 +1,9 @@
+const welcomePage = document.getElementById('welcomePage');
+const start = document.getElementById('buttonStart');
+
+
+//activate add resume button on click
+
+start.addEventListener('click', () => {
+    welcomePage.style.display = 'none';
+})
